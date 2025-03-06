9c15bfd9a23779f45da30e1185332d92624c70e1
JustABrian <107365004+JustSomeNoname@users.noreply.github.com>
2025-03-06 21:19:34 +0100

    Update EasterEggs.cs
    :3

--- Commands/Fun/EasterEggs.cs
+++ Commands/Fun/EasterEggs.cs
@@ -12,8 +12,9 @@ public class AGCEasterEggs : BaseCommandModule
     [Command("koni")]
     public async Task Koni(CommandContext ctx)
     {
-        await ctx.Channel.SendMessageAsync("POV <@244455692881100801>:");
+        await ctx.Channel.SendMessageAsync("POV <@443114493992763392>:");
+        await ctx.channel.SendMessageAsync("# Ich liebe Tomaten")
         await ctx.Channel.SendMessageAsync(
-            "https://cdn.discordapp.com/emojis/1116433698590556301.gif?size=256&quality=lossless");
+            "https://i.imgur.com/BTAcv4B.gif");
     }
 }
